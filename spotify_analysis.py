061211f223c73c9897e70bb99209fdb5706ae4cd
Yeo Duc Adonis <yeoadonis6@gmail.com>
2026-04-02 00:21:29 +0100

    Refactor simplifier_list and ranking functions
    Refactor simplifier_list and rank functions for better clarity and functionality.

--- spotify_analysis.py
+++ spotify_analysis.py
@@ -1,110 +1,57 @@
 
 #turn the list in the file into a simplier one
-def simplifier_list(orig_list) 
- for ligne in orig_list:
-     new_dict = {}
+def simplifier_list(orig_list):
+    simp_list = []
 
-     new_dict["track"] = ligne["master_metadata_track_name"]
-     new_dict["artist"] = ligne["master_metadata_album_artist_name"]
-     new_dict["album"] = ligne["master_metadata_album_album_name"]
-     new_dict["ms_played"] = ligne["ms_played"]
-     new_dict["timestamp"] = ligne["ts"]
+    for ligne in orig_list:
+        # filtre minimum
+        if ligne["master_metadata_track_name"] is None:
+            continue
 
-     simp_list.append(new_dict) 
+        if ligne["ms_played"] < 30000:
+            continue
 
-     return simp_list
+        new_dict = {
+            "track": ligne["master_metadata_track_name"],
+            "artist": ligne["master_metadata_album_artist_name"],
+            "album": ligne["master_metadata_album_album_name"],
+            "ms_played": ligne["ms_played"],
+            "timestamp": ligne["ts"]
+        }
 
-#Range les titres par nombre d'écoutes
-def rank_songs(data):
+        simp_list.append(new_dict)
+
+    return simp_list
+
+def rank(data, key_type="song", mode="count"):
     counts = {}
 
     for ligne in data:
-        key = (ligne["track"], ligne["artist"])
-        counts[key] = counts.get(key, 0) + 1
+
+        # définir la clé selon le type
+        if key_type == "song":
+            key = (ligne["track"], ligne["artist"])
+        elif key_type == "album":
+            key = (ligne["album"], ligne["artist"])
+        elif key_type == "artist":
+            key = ligne["artist"]
+
+        # définir la valeur selon le mode
+        if mode == "count":
+            value = 1
+        elif mode == "time":
+            value = ligne["ms_played"]
+
+        counts[key] = counts.get(key, 0) + value
 
     sorted_counts = sorted(
         counts.items(),
         key=lambda x: x[1],
         reverse=True
     )
+
     return sorted_counts
 
 
-#Range les titres par durée d'écoute.
-def rank_songsbis(data):
-    counts = {}
-
-    for ligne in data:
-        key = (ligne["track"], ligne["artist"])
-        counts[key] = counts.get(key, 0) +  ligne["timestamp"]
-
-    sorted_counts = sorted(
-        counts.items(),
-        key=lambda x: x[1],
-        reverse=True
-    )
-    return sorted_counts
-
-#Range les albums par nombre d'écoutes 
-def rank_albums(data):
-    counts = {}
-
-    for ligne in data:
-        key = (ligne["album"], ligne["artist"])
-        counts[key] = counts.get(key, 0) + 1
-
-    sorted_counts = sorted(
-        counts.items(),
-        key=lambda x: x[1],
-        reverse=True
-    )
-    
-    return sorted_counts
-
-#Range les albums par temps d'écoutes
-def rank_albumsbis(data):
-    counts = {}
-
-    for ligne in data:
-        key = (ligne["album"], ligne["artist"])
-        counts[key] = counts.get(key, 0) + ligne["timestamp"]
-
-    sorted_counts = sorted(
-        counts.items(),
-        key=lambda x: x[1],
-        reverse=True
-    )
-    return sorted_counts
- #Range les artistes par nombre d'écoutes
- def rank_artist(data):
-    counts = {}
-
-    for ligne in data:
-        key =  (ligne["artist"] )
-        counts[key] = counts.get(key, 0) + 1
-   
-
-    sorted_counts = sorted(
-        counts.items(),
-        key=lambda x: x[1],
-        reverse=True
-    )
-    return sorted_counts
-
-  #Range les artistes par temps écoutés
-  def rank_artistbis(data):
-    counts = {}
-
-    for ligne in data:
-        key =  (ligne["artist"] )
-        counts[key] = counts.get(key, 0) + ligne["timestamp"]
-   
-
-    sorted_counts = sorted(
-        counts.items(),
-        key=lambda x: x[1],
-        reverse=True
-    )
-    return sorted_counts
   
   
