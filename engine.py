cfe15c010820bc28db51cace32634cdd006163af
Yeo Duc Adonis <yeoadonis6@gmail.com>
2026-04-25 01:12:13 +0100

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,14 +1,11 @@
 from datetime import datetime, timedelta
+from collections import defaultdict
 
 
 def simplifier_list(orig_list):
     simp_list = []
-
     for ligne in orig_list:
-        if ligne["master_metadata_track_name"] is None:
-            continue
-
-        if ligne["ms_played"] < 30000:
+        if ligne.get("master_metadata_track_name") is None or ligne.get("ms_played", 0) < 30000:
             continue
 
         simp_list.append({
@@ -18,150 +15,144 @@ def simplifier_list(orig_list):
             "min_played": round(ligne["ms_played"] / 60000),
             "ts": datetime.fromisoformat(ligne["ts"].replace("Z", ""))
         })
-
     return simp_list
 
-
-
-
 def filter_by_period(data, period):
+    if not data: return []
     last_date = max(ligne["ts"] for ligne in data)
 
-    if period == "all":
-        return data
-
-    if period == "4weeks":
-        limit = last_date - timedelta(days=28)
-    elif period == "3months":
-        limit = last_date - timedelta(days=90)
-    elif period == "6months":
-        limit = last_date - timedelta(days=182)
-    elif period == "1year":
-        limit = last_date - timedelta(days=365)
-    elif period == "This year":
-        limit = datetime(last_date.year, 1, 1)
+    if period == "all": return data
+    
+    if period == "4weeks": limit = last_date - timedelta(days=28)
+    elif period == "3months": limit = last_date - timedelta(days=90)
+    elif period == "6months": limit = last_date - timedelta(days=182)
+    elif period == "1year": limit = last_date - timedelta(days=365)
+    elif period == "this year": limit = datetime(last_date.year, 1, 1)
     elif period == "last year":
         start = datetime(last_date.year - 1, 1, 1)
         end = datetime(last_date.year - 1, 12, 31)
         return [l for l in data if start <= l["ts"] <= end]
+    else: return data
 
     return [l for l in data if l["ts"] >= limit]
 
 
+from collections import defaultdict
+from datetime import datetime
 
-def rank(data, key_type="song", mode="count"):
-    counts = {}
+class ListeningContext:
+    def __init__(self, data):
+        self.data = data
+        self.songs = {}
+        self.artists = {}
+        self.albums = {}
+        self.rank_maps = {"count": {}, "time": {}}
+        
+        self._build_indices()
+        self._generate_all_rankings()
 
-    for ligne in data:
+    def _build_indices(self):
+        for ligne in self.data:
+            s_id = (ligne["track"], ligne["artist"])
+            ar_id = ligne["artist"]
+            al_id = (ligne["album"], ligne["artist"])
+            ts = ligne["ts"]
+            
+            duration = ligne["min_played"]
+            
+            if s_id not in self.songs:
+                self.songs[s_id] = {"count": 0, "min_played": 0, "first": ts, "last": ts, "album": ligne["album"]}
+            self.songs[s_id]["count"] += 1
+            self.songs[s_id]["min_played"] += duration
+            self.songs[s_id]["last"] = max(self.songs[s_id]["last"], ts)
+
+            if ar_id not in self.artists:
+                self.artists[ar_id] = {"count": 0, "min_played": 0, "tracks": defaultdict(int), "albums": defaultdict(int), "first": ts, "last": ts}
+            self.artists[ar_id]["count"] += 1
+            self.artists[ar_id]["min_played"] += duration
+            self.artists[ar_id]["tracks"][ligne["track"]] += 1
+            self.artists[ar_id]["albums"][ligne["album"]] += 1
+            self.artists[ar_id]["last"] = max(self.artists[ar_id]["last"], ts)
+
+            if al_id not in self.albums:
+                self.albums[al_id] = {"count": 0, "min_played": 0, "tracks": defaultdict(int), "first": ts, "last": ts}
+            self.albums[al_id]["count"] += 1
+            self.albums[al_id]["min_played"] += duration
+            self.albums[al_id]["tracks"][ligne["track"]] += 1
+            self.albums[al_id]["last"] = max(self.albums[al_id]["last"], ts)
+
+    def _generate_all_rankings(self):
+        from engine import filter_by_period 
+        periods = ["all", "4weeks", "3months", "6months", "1year", "this_year"]
+        
+        for p in periods:
+            p_data = filter_by_period(self.data, p)
+            for mode in ["count", "time"]:
+                self.rank_maps[mode][p] = {
+                    "song": self._build_rank_map(p_data, "song", mode),
+                    "artist": self._build_rank_map(p_data, "artist", mode),
+                    "album": self._build_rank_map(p_data, "album", mode)
+                }
+
+    def _build_rank_map(self, data, key_type, mode):
+        counts = defaultdict(int)
+        for l in data:
+            if key_type == "song": id_ = (l["track"], l["artist"])
+            elif key_type == "album": id_ = (l["album"], l["artist"])
+            else: id_ = l["artist"]
+            
+            counts[id_] += 1 if mode == "count" else l["min_played"]
+        
+        sorted_list = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+        return {item[0]: (i + 1, item[1]) for i, item in enumerate(sorted_list)}
+
+    def get_item_stats(self, identifier, key_type, mode="count"):
+        res = {}
+        
         if key_type == "song":
-            identifier = (ligne["track"], ligne["artist"])
+            base = self.songs.get(identifier)
+            if not base: return {}
+            res = {
+                "track": identifier[0], "artist": identifier[1], "album": base["album"],
+                "count": base["count"], "min_played": base["min_played"],
+                "first_listening": base["first"], "last_listening": base["last"]
+            }
+        elif key_type == "artist":
+            base = self.artists.get(identifier)
+            if not base: return {}
+            res = {
+                "artist": identifier, "count": base["count"], "min_played": base["min_played"],
+                "first_listening": base["first"], "last_listening": base["last"],
+                "number_of_songs": len(base["tracks"]), "number_of_albums": len(base["albums"]),
+                "top_tracks": sorted(base["tracks"].items(), key=lambda x: x[1], reverse=True),
+                "top_albums": sorted(base["albums"].items(), key=lambda x: x[1], reverse=True),
+                "songs_in_top_100": self._count_top_100_songs(identifier, mode)
+            }
         elif key_type == "album":
-            identifier = (ligne["album"], ligne["artist"])
-        else:
-            identifier = ligne["artist"]
+            base = self.albums.get(identifier)
+            if not base: return {}
+            res = {
+                "album": identifier[0], "artist": identifier[1],
+                "count": base["count"], "min_played": base["min_played"],
+                "first_listening": base["first"], "last_listening": base["last"],
+                "tracklist": sorted(base["tracks"].items(), key=lambda x: x[1], reverse=True)
+            }
 
-        if identifier not in counts:
-            counts[identifier] = 0
+        mapping = {
+            "all": "rank_alltime", "4weeks": "rank_4weeks",
+            "3months": "rank_3months", "6months": "rank_6months", "1year": "rank_1year"
+        }
 
-        if mode == "count":
-            counts[identifier] += 1
-        else:
-            counts[identifier] += ligne["min_played"]
+        for p_key, js_key in mapping.items():
+            rank_info = self.rank_maps[mode].get(p_key, {}).get(key_type, {}).get(identifier)
+            res[js_key] = rank_info[0] if rank_info else "N/A"
+            
+        return res
 
-    return sorted(counts.items(), key=lambda x: x[1], reverse=True)
-
-
-
-def build_rank_map(ranking):
-    return {identifier: i + 1 for i, (identifier, _) in enumerate(ranking)}
-
-
-
-def item_stats(data, identifier, key_type="song"):
-    stats = {}
-
-    data_all = data
-    data_4w = filter_by_period(data, "4weeks")
-    data_3m = filter_by_period(data, "3months")
-    data_6m = filter_by_period(data, "6months")
-    data_1y = filter_by_period(data, "1year")
-
-    rank_all = build_rank_map(rank(data_all, key_type))
-    rank_4w = build_rank_map(rank(data_4w, key_type))
-    rank_3m = build_rank_map(rank(data_3m, key_type))
-    rank_6m = build_rank_map(rank(data_6m, key_type))
-    rank_1y = build_rank_map(rank(data_1y, key_type))
-
-    if key_type == "song":
-        item_data = [l for l in data if (l["track"], l["artist"]) == identifier]
-    elif key_type == "album":
-        item_data = [l for l in data if (l["album"], l["artist"]) == identifier]
-    else:
-        item_data = [l for l in data if l["artist"] == identifier]
-
-    if not item_data:
-        return {}
-
-    listenings = []
-    track_counts = {}
-    album_counts = {}
-    unique_tracks = set()
-    unique_albums = set()
-
-    for ligne in item_data:
-        ts = ligne["ts"]
-        track = ligne["track"]
-        album = ligne["album"]
-
-        listenings.append(ts)
-
-        stats["count"] = stats.get("count", 0) + 1
-        stats["min_played"] = stats.get("min_played", 0) + ligne["min_played"]
-
-        track_counts[track] = track_counts.get(track, 0) + 1
-        album_counts[album] = album_counts.get(album, 0) + 1
-
-        unique_tracks.add(track)
-        unique_albums.add(album)
-
-        stats["artist"] = ligne["artist"]
-
-        if key_type == "song":
-            stats["track"] = track
-            stats["album"] = album
-        elif key_type == "album":
-            stats["album"] = album
-
-    stats["first_listening"] = min(listenings)
-    stats["last_listening"] = max(listenings)
-
-    stats["rank_alltime"] = rank_all.get(identifier)
-    stats["rank_4weeks"] = rank_4w.get(identifier)
-    stats["rank_3months"] = rank_3m.get(identifier)
-    stats["rank_6months"] = rank_6m.get(identifier)
-    stats["rank_1year"] = rank_1y.get(identifier)
-
-    if key_type == "song":
-        return stats
-
-    elif key_type == "album":
-        stats["tracklist"] = sorted(track_counts.items(), key=lambda x: x[1], reverse=True)
-        return stats
-
-    else:
-        stats["number_of_songs"] = len(unique_tracks)
-        stats["number_of_albums"] = len(unique_albums)
-
-        stats["top_tracks"] = sorted(track_counts.items(), key=lambda x: x[1], reverse=True)
-        stats["top_albums"] = sorted(album_counts.items(), key=lambda x: x[1], reverse=True)
-
-        rank_all_songs = rank(data_all, key_type="song")
-        top_100 = rank_all_songs[:100]
-
-        stats["songs_in_top_100"] = sum(
-            1 for item in top_100
-            if item[0][1] == identifier
-        )
-
-        return stats
- 
+    def _count_top_100_songs(self, artist_name, mode):
+        top_100_map = self.rank_maps[mode]["all"]["song"]
+        count = 0
+        for (track, artist), (rank, val) in top_100_map.items():
+            if rank <= 100 and artist == artist_name:
+                count += 1
+        return count
